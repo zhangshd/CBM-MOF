0de365bbe55af35b372d43033ac5de83339fdcbc
zhangshd <939620259@qq.com>
2025-09-13 10:44:10 +0800

    first commit

---
+++ src/moftransformer/module/heads.py
@@ -0,0 +1,170 @@
+# MOFTransformer version 2.0.0
+import torch.nn as nn
+import torch
+
+from transformers.models.bert.modeling_bert import (
+    BertConfig,
+    BertPredictionHeadTransform,
+)
+
+
+class Pooler(nn.Module):
+    def __init__(self, hidden_size, index=0):
+        super().__init__()
+        self.dense = nn.Linear(hidden_size, hidden_size)
+        self.activation = nn.Tanh()
+        self.index = index
+
+    def forward(self, hidden_states):
+        first_token_tensor = hidden_states[:, self.index]
+        pooled_output = self.dense(first_token_tensor)
+        pooled_output = self.activation(pooled_output)
+        return pooled_output
+
+
+class GGMHead(nn.Module):
+    """
+    head for Graph Grid Matching
+    """
+
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.fc = nn.Linear(hidden_size, 2)
+
+    def forward(self, x):
+        x = self.fc(x)
+        return x
+
+
+class MPPHead(nn.Module):
+    """
+    head for Masked Patch Prediction (regression version)
+    """
+
+    def __init__(self, hid_dim):
+        super().__init__()
+
+        bert_config = BertConfig(
+            hidden_size=hid_dim,
+        )
+        self.transform = BertPredictionHeadTransform(bert_config)
+        self.decoder = nn.Linear(hid_dim, 101 + 2)  # bins
+
+    def forward(self, x):  # [B, max_len, hid_dim]
+        x = self.transform(x)  # [B, max_len, hid_dim]
+        x = self.decoder(x)  # [B, max_len, bins]
+        return x
+
+
+class MTPHead(nn.Module):
+    """
+    head for MOF Topology Prediction
+    """
+
+    def __init__(self, hid_dim):
+        super().__init__()
+        self.fc = nn.Linear(hid_dim, 1397)  # len(assets/topology.json)
+
+    def forward(self, x):
+        x = self.fc(x)
+        return x
+
+
+class VFPHead(nn.Module):
+    """
+    head for Void Fraction Prediction
+    """
+
+    def __init__(self, hid_dim):
+        super().__init__()
+        self.bn = nn.BatchNorm1d(hid_dim)
+        self.fc = nn.Linear(hid_dim, 1)
+
+    def forward(self, x):
+        x = self.bn(x)
+        x = self.fc(x)
+        return x
+
+
+class RegressionHead(nn.Module):
+    """
+    head for Regression
+    """
+
+    def __init__(self, hid_dim):
+        super().__init__()
+        self.fc_out = nn.Linear(hid_dim, 1)
+
+    def forward(self, x):
+        x = self.fc_out(x)
+        return x
+
+
+class ClassificationHead(nn.Module):
+    """
+    head for Classification
+    """
+
+    def __init__(self, hid_dim, n_classes):
+        super().__init__()
+
+        if n_classes == 2:
+            self.fc_out = nn.Linear(hid_dim, 1)
+            self.binary = True
+        else:
+            self.fc_out = nn.Linear(hid_dim, n_classes)
+            self.binary = False
+
+    def forward(self, x):
+        x = self.fc_out(x)
+
+        return x, self.binary
+
+
+class MOCHead(nn.Module):
+    """
+    head for Metal Organic Classification
+    """
+
+    def __init__(self, hid_dim):
+        super().__init__()
+        self.fc = nn.Linear(hid_dim, 1)
+
+    def forward(self, x):
+        """
+        :param x: graph_feats [B, graph_len, hid_dim]
+        :return: [B, graph_len]
+        """
+        x = self.fc(x)  # [B, graph_len, 1]
+        x = x.squeeze(dim=-1)  # [B, graph_len]
+        return x
+
+class NonLinearHead(nn.Module):
+    """Head for simple tasks."""
+
+    def __init__(
+        self,
+        input_dim,
+        out_dim,
+        hidden=None,
+        norm="none",
+    ):
+        super().__init__()
+        hidden = input_dim if not hidden else hidden
+        self.linear1 = nn.Linear(input_dim, hidden)
+        self.linear2 = nn.Linear(hidden, out_dim)
+        self.activation_fn = nn.Softplus()
+        if norm == "batch":
+            self.norm = nn.BatchNorm1d(hidden)
+        elif norm == "layer":
+            self.norm = nn.LayerNorm(hidden)
+        else:
+            self.norm = nn.Identity()
+
+    def forward(self, x):
+
+        x = self.linear1(x)
+        x = self.norm(x)
+        x = self.activation_fn(x)
+        x = self.linear2(x)
+        return x
